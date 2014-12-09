c3f4595c8776c3af002ec99d9748b156d36f3aab
DomWalker <negate4ever@gmail.com>
2014-12-08 19:50:18 -0500

    First Commit

---
+++ src/CheckExistingDirectory.java
@@ -0,0 +1,25 @@
+/**
+ * Created by DWalker on 2014-12-08.
+ */
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class CheckExistingDirectory {
+
+    public static void main(String[] args)throws IOException{
+        Path basePath = Paths.get("E:\\Testing\\DemoHub\\FieldDesgner_4.16");
+        Path endPath = Paths.get("E:\\Testing\\DemoHub\\Test1");
+        CopyOption[] options = new CopyOption[]{
+                StandardCopyOption.REPLACE_EXISTING,
+                StandardCopyOption.COPY_ATTRIBUTES
+        };
+        if (!Files.exists(endPath)){
+            System.out.println("Directory Doesn't Exist");
+            System.out.println("Creating Directory");
+            Files.createDirectories(endPath);
+        }
+
+        Files.copy(basePath,endPath, options);
+    }
+}
