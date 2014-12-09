0812120090fa17737ebc71f49dfe09082d8f09ae
DomWalker <negate4ever@gmail.com>
2014-12-09 18:07:14 -0500

    Added variables about existence of base data

--- src/CheckExistingDirectory.java
+++ src/CheckExistingDirectory.java
@@ -10,12 +10,14 @@ public class CheckExistingDirectory {
     public static void main(String[] args)throws IOException{
         Path basePath = Paths.get("E:\\Testing\\DemoHub\\FieldDesgner_4.16");
         Path endPath = Paths.get("E:\\Testing\\DemoHub\\Test1");
+        boolean copyFrom = true;
         CopyOption[] options = new CopyOption[]{
                 StandardCopyOption.REPLACE_EXISTING,
                 StandardCopyOption.COPY_ATTRIBUTES
         };
         if (!Files.exists(basePath)){
             System.out.println("Reports Install is not present! Ending Process");
+            copyFrom = false;
             return;
         } else if (!Files.exists(endPath)){
             System.out.println("Directory Doesn't Exist");
