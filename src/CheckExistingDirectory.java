e6ee783a5c8448aa863a851572c5575fce29d402
DomWalker <negate4ever@gmail.com>
2014-12-31 20:18:19 -0500

    Added Folder Picker Class to Select Destination for New Files

--- src/CheckExistingDirectory.java
+++ src/CheckExistingDirectory.java
@@ -3,12 +3,12 @@
  */
 
 import java.io.IOException;
-import java.nio.file.*;
+        import java.nio.file.*;
 
 public class CheckExistingDirectory {
 
-    public static void main(String[] args)throws IOException{
-        Path basePath = Paths.get("E:\\Testing\\DemoHub\\FieldDesgner_4.16");
+    public static void main(String[] args) throws Exception {
+        Path basePath = Paths.get("E:\\Testing\\DemoHub\\FieldDesigner_4.16");
         Path endPath = Paths.get("E:\\Testing\\DemoHub\\Test1");
         boolean copyFrom = true;
         CopyOption[] options = new CopyOption[]{
@@ -25,7 +25,13 @@ public class CheckExistingDirectory {
             Files.createDirectories(endPath);
         }
 
-        
+        FolderPicker ChoosePath = new FolderPicker();
+        try {
+            ChoosePath.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         Files.copy(basePath,endPath, options);
     }
 }
---
+++ src/FolderPicker.java
@@ -0,0 +1,68 @@
+/**
+ * Created by DWalker on 2014-12-31.
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.File;
+
+public class FolderPicker  {
+    String selectedPath;
+    Window primaryStage = null;
+
+    @Override
+    public void start() throws Exception {
+        final Label labelSelectedDirectory = new Label();
+        Button openDirectoryButton = new Button("Open Directory Chooser");
+        Button cancelButton = new Button("Cancel");
+        openDirectoryButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                DirectoryChooser directoryChooser = new DirectoryChooser();
+                File selectedDirectory = directoryChooser.showDialog(primaryStage);
+                if (selectedDirectory == null) {
+                    labelSelectedDirectory.setText("No Directory Selected");
+                } else {
+                    labelSelectedDirectory.setText(selectedDirectory.getAbsolutePath());
+                }
+                String selectedPath = selectedDirectory.getAbsolutePath();
+            }
+        });
+
+
+        cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                primaryStage.close();
+            }
+        });
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(labelSelectedDirectory, openDirectoryButton, cancelButton);
+        StackPane root = new StackPane();
+        root.getChildren().add(vBox);
+        ;
+
+        Scene scene = new Scene(root, 200 ,200);
+
+        primaryStage.setTitle("New Reports Installer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    /*public static void main(String[] args) {
+        launch(args);
+
+    }*/
+}
